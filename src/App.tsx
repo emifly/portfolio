24551971cb3e2727c8a211c66523b0d95742934b
Emily Flynn <emily.flynn@softwire.com>
2024-02-23 17:07:44 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css"
+
+function App() {
+  return (
+    <main>
+      <div className="about-me"></div>
+      <div className="my-projects"></div>
+    </main>
+  )
+}
+
+export default App
