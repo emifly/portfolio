05b183e5509bf1c30d0f638e0c183bb75c46df68
Emily Flynn <emily.flynn@softwire.com>
2024-02-23 17:37:16 +0000

    Add details about me

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import "./App.css"
 import { projects } from "./data/projects"
+import laptop from "./assets/laptop.png"
 
 function App() {
   return (
     <main>
-      <div className="about-me"></div>
+      <div className="about-me">
+        <h1>Emily Flynn</h1>
+        <p>Full-stack web developer</p>
+        <img src={laptop} alt="Laptop" className="about-me-image" />
+      </div>
       <div className="my-projects">
         {projects.map((project) => (
           <a href={project.link} className="project-tile">
